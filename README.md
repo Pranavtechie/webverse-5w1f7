65636e864b95c95f366ba1ad426fefe4c9cf7e8f
Emira Quent <emiraquent@gmail.com>
2023-06-23 17:51:00 +0100

    hrithikesh first push

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
-# webverse-5w1f7
++gi# webverse-5w1f7
 make a front-end
-
-
-# Frontend Packages
-
-1. Using Next.js Framework (React.js)
-
-2. Using Tailwind CSS (Shadcn/ui)
-
-3. useSWR (Handling Network Requests)
-
--- client/src/pages/student/register.jsx
+++ client/src/pages/student/register.jsx
@@ -35,7 +35,11 @@ export default function Register() {
 	return (
 		<>
 			<h1>Login Page</h1>
-
+			{/*  
+			<div class="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+  			<div class="sm:mx-auto sm:w-full sm:max-w-md">*/}
+			<div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+    <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
 			<form onSubmit={handleStudentLogin}>
 				<div className="flex flex-col m-2 space-y-5">
 					<input
@@ -123,6 +127,8 @@ export default function Register() {
 					/>
 				</div>
 			</form>
+			</div>
+			</div>
 		</>
 	);
 }
