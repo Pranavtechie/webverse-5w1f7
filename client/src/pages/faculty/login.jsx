3d2e4a3e7dbe84da38223fd0ef6f48e501430c8a
Emira Quent <emiraquent@gmail.com>
2023-06-23 19:55:07 +0100

    hrithik faculty login page functionality push

---
+++ client/src/pages/faculty/login.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { API_URL, setAuthToken } from "@/constants";
+export default function Login() {
+	const router = useRouter();
+	const [employeeId, setEmployeeId] = useState("");
+	const [password, setPassword] = useState("");
+
+	async function handleFacultyLogin(e) {
+		e.preventDefault();
+
+		console.log(e);
+		console.log({ employeeId, password });
+
+		let response = await fetch(`${API_URL}/faculty/auth/login`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ empId: employeeId, password }),
+		});
+		let data = await response.json();
+
+		setAuthToken(data.token);
+	}
+	return (
+		<>
+			<h1>Login Page</h1>
+
+			<form onSubmit={handleFacultyLogin}>
+				<div className="flex flex-col m-2 space-y-5">
+					<input
+						type="text"
+						placeholder="Employee Id"
+						className="input input-bordered w-full max-w-xs"
+						value={employeeId}
+						min={5}
+						max={5}
+						onChange={(e) => setEmployeeId(e.target.value)}
+					/>
+					<input
+						type="password"
+						placeholder="Password"
+						className="input input-bordered w-full max-w-xs"
+						value={password}
+						min={8}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+
+					<input
+						type="submit"
+						className="btn btn-primary"
+						value="Login"
+					/>
+				</div>
+			</form>
+		</>
+	);
+}
