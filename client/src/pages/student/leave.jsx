29fd679e0a26e291607ec96f920034b93b2f47a8
Pranav <pranav.techiegeek@gmail.com>
2023-06-23 23:56:39 +0530

    feat(leave functionality)

--- client/src/pages/student/leave.jsx
+++ client/src/pages/student/leave.jsx
@@ -4,6 +4,12 @@ import { useState, useEffect, useRef } from "react";
 import { API_URL, getAuthToken } from "@/constants";
 
 export default function Leave() {
+	const [newLeaveData, setNewLeaveData] = useState({
+		leaveType: "",
+		leaveDate: "",
+		leaveTime: "",
+		leaveDuration: "",
+	});
 	const leaveRef = useRef();
 
 	useEffect(() => {
@@ -25,6 +31,22 @@ export default function Leave() {
 
 		fetchDefaultData();
 	}, []);
+
+	async function applyForLeave() {
+		console.log(newLeaveData);
+
+		let response = await fetch(`${API_URL}/student/leave`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${getAuthToken()}`,
+			},
+			body: JSON.stringify(newLeaveData),
+		});
+
+		let data = await response.json();
+		console.log({ data });
+	}
 	return (
 		<>
 			<h1>Leave Page</h1>
@@ -38,13 +60,106 @@ export default function Leave() {
 			{/* Open the modal using ID.showModal() method */}
 			<dialog id="my_modal_1" ref={leaveRef} className="modal">
 				<form method="dialog" className="modal-box">
-					<h3 className="font-bold text-lg">Hello!</h3>
-					<p className="py-4">
-						Press ESC key or click the button below to close
-					</p>
+					<h2 className="text-2xl text-center font-semibold">
+						Apply for Leave
+					</h2>
+
+					{/* Select Leave Type Input */}
+					<div className="form-control max-w-xs">
+						<label className="label">
+							<span className="label-text">
+								Select Leave Type
+							</span>
+						</label>
+						<select
+							id="leave-type"
+							value={newLeaveData.leaveType}
+							className="select select-accent max-w-xs"
+							onChange={(event) => {
+								setNewLeaveData((d) => {
+									return {
+										...d,
+										leaveType: event.target.value,
+									};
+								});
+							}}
+						>
+							<option value="parent">w/ Parent Leave</option>
+							<option value="outing">Outing</option>
+							<option value="home-town">Home Town</option>
+						</select>
+					</div>
+
+					<div className="form-control max-w-xs">
+						<label className="label">
+							<span className="label-text text-sm mb-0">
+								Leave Date
+							</span>
+						</label>
+						<input
+							type="date"
+							className="input input-accent"
+							id="leave-date"
+							value={newLeaveData.leaveDate}
+							onChange={(e) => {
+								setNewLeaveData((d) => {
+									return {
+										...d,
+										leaveDate: e.target.value,
+									};
+								});
+							}}
+						/>
+					</div>
+
+					<div className="form-control max-w-xs">
+						<label className="label">
+							<span className="label-text text-sm mb-0">
+								Leave Time
+							</span>
+						</label>
+						<input
+							type="time"
+							className="input input-accent"
+							id="leave-time"
+							value={newLeaveData.leaveTime}
+							onChange={(e) => {
+								setNewLeaveData((d) => {
+									return { ...d, leaveTime: e.target.value };
+								});
+							}}
+						/>
+					</div>
+
+					<div className="form-control max-w-xs">
+						<label className="label">
+							<span className="label-text text-sm mb-0">
+								Leave Duration
+							</span>
+						</label>
+						<input
+							type="number"
+							className="input input-accent"
+							id="duration"
+							value={newLeaveData.leaveDuration}
+							onChange={(e) => {
+								setNewLeaveData((d) => {
+									return {
+										...d,
+										leaveDuration: e.target.value,
+									};
+								});
+							}}
+						/>
+					</div>
+
 					<div className="modal-action">
-						{/* if there is a button in form, it will close the modal */}
-						<button className="btn">Close</button>
+						<button
+							className="btn btn-primary"
+							onClick={applyForLeave}
+						>
+							Apply for Leave
+						</button>
 					</div>
 				</form>
 			</dialog>
